2742dc9850375a66c4cc42189c3b4f81ac36e1d8
m9ts <mat3us.gois@gmail.com>
2025-05-30 16:29:00 -0300

    Configurando a porta do servidor

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Vasco da Gama");
+import express from "express";
+
+const app = express();
+const PORT = 3090;
+
+app.use(express.json());
+
+//Importar e implementar rotas
+
+
+app.use("/library/usuarios", );
+app.use("/library/livros", );
+app.use("/library/estoque", );
+app.use("/library/emprestimos", );
+
+
+app.listen(PORT, () => {console.log(`Servidor ouvindo em http://localhost:${PORT}/library`);
+});
