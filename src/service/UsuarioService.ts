f182fedad26ddf1d7ee09fcfda6bc3a3c228d1ca
m9ts <mat3us.gois@gmail.com>
2025-06-05 19:03:20 -0300

    Ajustando pasta service

---
+++ src/service/UsuarioService.ts
@@ -0,0 +1,34 @@
+import { UsuarioRepository } from "../repository/UsuarioRepository";
+import { Usuario } from "../model/Usuario";
+
+export class UsuarioService {
+  private usuarioRepository: UsuarioRepository;
+
+  constructor() {
+    this.usuarioRepository = new UsuarioRepository();
+  }
+
+  cadastrar(usuario: Usuario): boolean {
+    if (this.usuarioRepository.buscarCPF(usuario.cpf)) {
+      return false; 
+    }
+    this.usuarioRepository.cadastrar(usuario);
+    return true;
+  }
+
+  buscarCPF(cpf: string): Usuario | undefined {
+    return this.usuarioRepository.buscarCPF(cpf);
+  }
+
+  listar(): Usuario[] {
+    return this.usuarioRepository.listar();
+  }
+
+  atualizar(cpf: string, nome?: string, ativo?: boolean, categoria_id?: number, curso_id?: number, suspensao?: Date): boolean {
+    return this.usuarioRepository.atualizar(cpf, nome, ativo, categoria_id, curso_id, suspensao);
+  }
+
+  remover(cpf: string): boolean {
+    return this.usuarioRepository.remover(cpf);
+  }
+}
