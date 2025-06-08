25bcaa43e6d029448220ca42633ebf861f45557b
m9ts <mat3us.gois@gmail.com>
2025-06-07 21:58:04 -0300

    Correção na lógica de máximo de empréstimos

--- src/service/EmprestimoService.ts
+++ src/service/EmprestimoService.ts
@@ -16,12 +16,23 @@ export class EmprestimoService {
 
     cadastrar(emprestimo: Emprestimo): boolean {
         const usuario = this.usuarioService.buscarCPF(emprestimo.usuario_id);
-        if (!usuario || !usuario.ativo || usuario.suspensao) return false;
+        if (!usuario || !usuario.ativo || (usuario.suspensao && usuario.suspensao > new Date())) return false;
 
         const estoque = this.estoqueService.buscarCodigo(emprestimo.codigo);
         if (!estoque || estoque.quantidade <= estoque.quantidade_emprestimo) return false;
 
-        let diasEmprestimo = usuario.categoria_id === 1 ? 40 : usuario.categoria_id === 2 && usuario.curso_id === estoque.codigo ? 30 : 15;
+        const emprestimosAtuais = this.buscarUsuarioId(emprestimo.usuario_id);
+
+        if (usuario.categoria_id === 1 && emprestimosAtuais.length >= 5) return false; 
+        if (usuario.categoria_id === 2 && emprestimosAtuais.length >= 3) return false; 
+
+        let diasEmprestimo = 15;
+
+        if (usuario.categoria_id === 1) {
+            diasEmprestimo = 40;
+        } else if (usuario.categoria_id === 2 && usuario.curso_id === estoque.codigo) {
+            diasEmprestimo = 30;
+        }
 
         const dataDevolucao = new Date();
         dataDevolucao.setDate(dataDevolucao.getDate() + diasEmprestimo);
@@ -57,6 +68,10 @@ export class EmprestimoService {
             if (usuario) {
                 usuario.suspensao = new Date();
                 usuario.suspensao.setDate(usuario.suspensao.getDate() + emprestimo.atraso_dias * 3);
+
+                if (usuario.suspensao.getTime() - new Date().getTime() > 60 * 24 * 60 * 60 * 1000) {
+                    usuario.ativo = false; 
+                }
             }
         }
 
@@ -66,9 +81,6 @@ export class EmprestimoService {
     remover(id: number): boolean {
         const emprestimo = this.buscarPorId(id);
         if (!emprestimo) return false;
-
-        const usuario = this.usuarioService.buscarCPF(emprestimo.usuario_id);
-        if (!usuario || usuario.suspensao) return false; 
 
         return this.emprestimoRepository.remover(id);
     }
