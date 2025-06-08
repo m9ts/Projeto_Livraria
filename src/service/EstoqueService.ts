0229db101a6a4319b9e1c64462e36ed8900567ac
m9ts <mat3us.gois@gmail.com>
2025-06-07 22:01:42 -0300

    Ajuste no bloqueio de remoção de livro se ele estiver emprestado

--- src/service/EstoqueService.ts
+++ src/service/EstoqueService.ts
@@ -40,7 +40,7 @@ export class EstoqueService {
   remover(codigo: number): boolean {
     const estoque = this.estoqueRepository.buscarCodigo(codigo);
 
-    if (!estoque || estoque.quantidade_emprestimo > 0) return false;
+    if (!estoque || estoque.quantidade_emprestimo > 0) return false; 
 
     return this.estoqueRepository.remover(codigo);
   }
