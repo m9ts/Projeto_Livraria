6a2b084782d42c4bf3ee72cf94a42f7868e1f04b
m9ts <mat3us.gois@gmail.com>
2025-06-23 11:37:29 -0300

    Ajustes em app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { UsuarioController } from "./controller/UsuarioController";
 import { LivroController } from "./controller/LivroController";
 import { EstoqueController } from "./controller/EstoqueController";
 import { EmprestimoController } from "./controller/EmprestimoController";
+import { CategoriaUsuario } from "./model/CategoriaUsuario";
 
 const app = express();
 const PORT = 3090;
@@ -13,7 +14,11 @@ const usuarioController = new UsuarioController();
 const livroController = new LivroController();
 const estoqueController = new EstoqueController();
 const emprestimoController = new EmprestimoController();
-
+/*
+const categoriaUsuarioController = new CategoriaUsuarioController();
+const categoriaCursoController = new CategoriaCursoController();
+const categoriaLivroController = new CategoriaLivroController();
+*/
 app.post("/library/usuarios", usuarioController.cadastrar.bind(usuarioController));
 app.get("/library/usuarios", usuarioController.listar.bind(usuarioController));
 app.get("/library/usuarios/:cpf", usuarioController.buscarCPF.bind(usuarioController));
@@ -38,9 +43,8 @@ app.get("/library/emprestimos/:id", emprestimoController.buscarPorId.bind(empres
 app.put("/library/emprestimos/:id/devolucao", emprestimoController.atualizarDataEntrega.bind(emprestimoController));
 app.delete("/library/emprestimos/:id", emprestimoController.remover.bind(emprestimoController));
 
-//Implementar endpoints de catálogo
-app.get("/categorias-usuario");
-app.get("/categorias-livro");
-app.get("/cursos");
+app.get("library/catalogos/categorias-usuario", );
+app.get("library/catalogos/categorias-livro");
+app.get("lirary/catalogs/cursos");
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}/library`));
